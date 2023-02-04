d3a6953a612fd5c36ea45fee99e0ac732b4523cd
erhanatalan <erhanatalan2013@gmail.com>
2023-02-04 14:40:30 +0300

    animation

--- weather.js
+++ weather.js
@@ -55,6 +55,73 @@ const getWeatherDataFromApi = async () =>{
         dataType : "json",
         success: (response) => {
             console.log(response)
+            const {main, sys, weather, name} = response
+            const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+            //alternative iconUrl
+            const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+            const cityCardList = listJQ.find("city")
+            const cityCardListArray = cityCardList.get() //? array e ceviriyor
+            if(cityCardListArray.length > 0 ){
+                const filteredArray = cityCardListArray.filter(card => $(card).find("span").text() == name )
+                if(filteredArray.length > 0){
+                    msgJQ.text(`You already know the weather for ${name}, Please search for another city 😉`);
+                    //styling
+                    msgJQ.css({ "color": "red", "text-decoration": "underline" });
+                    setTimeout( ()=>{
+                        msgJQ.text(``)
+                    },3000)
+                    formJQ.trigger("reset")
+                    return
+                }
+            }
+
+
+            const createdLi = $("<li></li>")
+            createdLi.addClass("city");
+            createdLi.html(`
+            <h2 class="city-name" data-name="${name}, ${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}
+                <sup>°C</sup>
+            </div>
+            <figure>
+                <img class="city-icon" src="${iconUrlAWS}">
+                <figcaption>${weather[0].description}</figcaption>
+            </figure>
+        `)
+
+            listJQ.prepend(createdLi)
+            // formJQ[0].reset() alternative
+            formJQ.trigger("reset")
+
+            // animation
+            $("city").click((e)=>{
+                $(e.target)
+                    .animate({left:"50px"})
+                    .animate({left:"150px"})
+                    .animate({left:"250px"})
+                    .animate({left:"150px"})
+                    .animate({left:"50px"})
+                    .animate({left:"0px"})
+            })
+            //jquery slideup-slidedown
+            $(".city img").click((e) => {
+                //convert js to jquery
+                //jquery chaning
+                $(e.target)
+                    .slideUp(1500)
+                    .slideDown(1500)
+            });
+
+            //jquery slideup-slidedown
+            // $(".city img").click((e) => {
+            //     $(e.target)
+            //         .hide()
+            // });
+
         },
         beforeSend : (request) => {
             //parameter => headers 
@@ -62,6 +129,13 @@ const getWeatherDataFromApi = async () =>{
         },
         complete : ()=>{},
         error : (XMLHttpRequest) =>{
+            msgJQ.text(`${XMLHttpRequest.status} ${XMLHttpRequest.statusText}`);
+            //styling
+            msgJQ.css({ "color": "red", "text-decoration": "underline" });
+            setTimeout( ()=>{
+                msgJQ.text(``)
+            },3000)
+            formJQ.trigger("reset")
             //  error handling
         }
     })
